060a300f9fd471a355bdc2355fcd4f2aee44d7b3
Dylan Lgrng <lagrangedylan@gmail.com>
2026-01-29 15:31:51 +0100

    push 001.1

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// IMPORTANT: this makes the app work at https://dylanlagrange.fr/uxrepublic/
-export default defineConfig({
-  plugins: [react()],
-  base: '/uxrepublic/',
-})
